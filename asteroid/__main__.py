01be27a564d1e16ce852c623086384de7b9e4dc3
Benjamin Anderson <benjaminanderson98@live.com>
2021-10-14 17:13:43 -0600

    Modified: game.__init__, constants, __main__

---
+++ asteroid/__main__.py
@@ -0,0 +1,13 @@
+import arcade
+from game import constants
+from game.director import Director
+
+def main():
+    """ The starting point of the application.
+    """
+    director = Director(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
+    director.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
--- asteroid/game/constants.py
+++ asteroid/game/constants.py
@@ -1,4 +1,4 @@
-
+from os import path
 
 SPRITE_SCALING = 0.3
 
@@ -7,5 +7,7 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Batter - Group 07"
 
 MOVEMENT_SPEED = 20
+ENEMY_SPEED = 10
 
+ROOT_DIRECTORY = path.abspath(path.join(path.dirname(__file__),".."))
 RESOURCE_DIRECTORY = path.join(ROOT_DIRECTORY, "resources")
