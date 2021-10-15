296a424f73804762784b00b43c2c110fff67c3b7
Benjamin Anderson <benjaminanderson98@live.com>
2021-10-15 16:35:41 -0600

    added methods to player_ship

--- asteroid/game/asteroid.py
+++ asteroid/game/asteroid.py
@@ -1,10 +1,19 @@
 
-class Asteroid:
+from game.floating_object import FloatingObject
+
+class Asteroid(FloatingObject):
     """ A hunk of space rock (in addition to other elements) that try to make the player's ship have a bad day.
     
         Stereotypes:
-            TODO
+            Information Holder
         
         Attributes:
-            TODO
+            NONE
     """
+
+    def update(self):
+        """ Handles what happens on update
+        
+            Args:
+                self (Asteroid): An instance of Asteroid.
+        """
--- asteroid/game/director.py
+++ asteroid/game/director.py
@@ -1,5 +1,7 @@
 
-class Director:
+import arcade
+
+class Director(arcade.Window):
     """ The main controller class. Handles the flow of the program.
     
         Stereotypes:
