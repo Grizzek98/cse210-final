e4689d5a4010a0c90a5dd1891affb370274ac0ff
Benjamin Anderson <benjaminanderson98@live.com>
2021-10-15 16:52:08 -0600

    added methods to director

--- asteroid/game/director.py
+++ asteroid/game/director.py
@@ -5,8 +5,66 @@ class Director(arcade.Window):
     """ The main controller class. Handles the flow of the program.
     
         Stereotypes:
-            TODO
+            Controller
         
         Attributes:
-            TODO
+            NONE
     """
+
+    def __init__(self, width, height, title):
+        """ The constructor of the class. Initializes the classes attributes and opens the arcade window.
+        
+            Args:
+                self (ArcadeDirector): An instance of ArcadeDirector.
+                width (constant): The width of the arcade screen.
+                height (constant): The height of the arcade screen.
+                title (constant): The title of the arcade screen.
+        """
+
+    def setup(self):
+        """ Handles the initial setup of the game.
+        
+            Args:
+                TODO
+        """
+        pass
+
+    def on_update(self):
+        """ Handles what happens every update.
+        
+            Args:
+                TODO
+        """
+        pass
+
+    def on_draw(self):
+        """ Handles what happens every time the screen is refreshed.
+        
+            Args:
+                TODO
+        """
+        pass
+
+    def on_key_press(self, key, modifiers):
+        """ Handles what happens when a key is pressed.
+        
+            Args:
+                TODO
+        """
+        pass
+
+    def on_key_release(self, key, modifiers):
+        """ Handles what happens when a key is released.
+        
+            Args:
+                TODO
+        """
+        pass
+
+    def check_collision(self):
+        """ Checks for collision between objects on the screen.
+        
+            Args:
+                TODO
+        """
+        pass
