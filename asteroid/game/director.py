0ca0e716717a38a4ed431956a2dcea87c613e651
artable <r.jonah.deal@gmail.com>
2021-10-19 21:10:33 -0600

    modified constants and updated documentation

--- asteroid/game/director.py
+++ asteroid/game/director.py
@@ -126,7 +126,7 @@ class Director(arcade.Window):
         """
         self.keyboard_control.key_release(key, self.player_ship_sprite)
     
-    def shot(self):
+    def shot(self): #TODO create constructor returning new shots
         """Creates a projectile
 
         Args:
