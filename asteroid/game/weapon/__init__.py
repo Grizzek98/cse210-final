3e8e45189429f0a572f36f5c4fa33f4c6f487817
Benjamin Anderson <benjaminanderson98@live.com>
2021-10-20 17:35:09 -0600

    created weapon module.

---
+++ asteroid/game/weapon/__init__.py
@@ -0,0 +1,5 @@
+""" Contains code from the Weapon module"""
+
+from game.weapon.weapon_base import WeaponBase
+from game.weapon.basic_laser import BasicLaser
+from game.weapon.projectile import Projectile
