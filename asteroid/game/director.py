1e7f247772f397c3647e6def75a9c70cdc773bd0
artable <r.jonah.deal@gmail.com>
2021-10-20 22:31:17 -0600

    Made a script for the game to progressively become harder as the game goes on in noticable bursts. said script can be used to implement different enemies at different times as well.

--- asteroid/game/director.py
+++ asteroid/game/director.py
@@ -14,6 +14,7 @@ from game.asteroid import Asteroid
 from game.keyboard_control import KeyboardControl
 from game.projectile import Projectile
 from game.score import Score
+from game.script import GameScript
 
 
 class Director(arcade.View):
@@ -55,8 +56,8 @@ class Director(arcade.View):
         self.spawn_enemy = spawn.SpawnEnemy()
         self.spawn_asteroid = spawn.SpawnAsteroid()
         self.score = Score()
-        self.spawn_asteroid_control = 0
-        self.asteroid_spawn_rate = 1
+        self.script = GameScript()
+
 
     def setup(self):
         """ Handles the initial setup of the game.
@@ -90,23 +91,14 @@ class Director(arcade.View):
             self.player_ship_sprite.angle)
             self.player_projectile_list.append(new_shot)
             self.play_shoot_sound()
+        if len(self.asteroid_list) < self.script.enemy_max :
+            self.asteroid_list.append(self.spawn_asteroid.spawn())
+        self.script.update(delta_time)
         self.player_projectile_list.on_update(delta_time)
         self.sprite_list.on_update(delta_time) #<3 arcade
         self.asteroid_list.on_update(delta_time)
         self.check_collision()
         self.check_remove_sprite()
-
-        #Spawn randomly asteroids
-        self.spawn_asteroid_control += delta_time
-        if self.spawn_asteroid_control >= self.asteroid_spawn_rate:
-            self.asteroid_list.append(self.spawn_asteroid.spawn())
-            self.spawn_asteroid_control = 0
-
-            if self.asteroid_spawn_rate > constants.MAX_SPAWN_RATE:
-                self.asteroid_spawn_rate -= 0.05
-            else:
-                self.asteroid_spawn_rate = constants.MAX_SPAWN_RATE
-
 
     def on_draw(self):
         """ Handles what happens every time the screen is refreshed.
